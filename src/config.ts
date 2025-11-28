4bd36da18a43592136c0eb54b969cf49e17d06bf
Adrian Silva <grsadrian01@gmail.com>
2025-11-27 21:45:07 -0300

    chore: initial project setup

---
+++ src/config.ts
@@ -0,0 +1,36 @@
+export interface Config {
+  baseUrl: string;
+}
+
+export interface Indicator {
+  name: string;
+  route: string;
+}
+
+export const config: Config = {
+  baseUrl: "https://www.cepea.org.br/br/",
+};
+
+export const indicator: Indicator[] = [
+    {name: "acucar", route: "acucar"},
+    {name: "algodao", route: "algodao"},
+    {name: "arroz", route: "arroz"},
+    {name: "bezerro", route: "bezerro"},
+    {name: "boi", route: "boi"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+    {name: "algodao", route: "algodao"},
+]
