f39fcbf4196e7ad57181955dc21d65f42af0c9c6
Adrian Silva <grsadrian01@gmail.com>
2025-12-02 21:21:14 -0300

    feat(scrapper): scrap da tabela de indicadores - realiza scrap na tabela de todos os indicadores - cria classe de serviço para transformar dados brutos em dados tratados

--- src/classes/Scrapper.ts
+++ src/classes/Scrapper.ts
@@ -1,21 +1,32 @@
 import puppeteer, { Browser, Page } from "puppeteer";
 import { config, indicator } from "../config/config.js";
+import { ParsedData, ProcessData } from "../services/ProcessData.js";
 
 export class Scrapper {
   url = config.baseUrl;
-  indicators = indicator
+  indicators = indicator;
+  processor = new ProcessData();
+  parsedData!: ParsedData[];
+  rawData?: string;
   browser!: Browser;
   page!: Page;
 
   constructor() {}
 
   async start(): Promise<void> {
-    this.browser = await puppeteer.launch({ headless: false });
+    this.browser = await puppeteer.launch();
     this.page = await this.browser.newPage();
   }
 
   async scrap(): Promise<void> {
-    await this.page.goto(this.url);
+    for (let i = 0; i++; this.indicators.length) {
+      await this.page.goto(`${this.url}${this.indicators[0].route}.aspx`);
+      this.rawData = await this.page.evaluate(() => {
+        let element = document.querySelector("tbody");
+        return element?.innerHTML;
+      });
+      this.processor.processData(this.rawData);
+    }
   }
 
   async close(): Promise<void> {
